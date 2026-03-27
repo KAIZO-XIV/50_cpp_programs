a92689420368734056df7d08b7df75b563b9e95f
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-27 16:36:36 +0530

    Modify hello world program to include additional output
    Updated the program to print an additional message.

--- src/01_hello.cpp
+++ src/01_hello.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+/*
+  Program: Hello 
+  Description: Print hello world onthe screen.
+  Date: 27-Mar-2026
+  */
+
+#include <iostream> //including library
 using namespace std
+
+// main function
 int main ()
 {
   cout<<"Hello World";
-return 0;
+  cout<<"Working on CPP is fantastic";
+  return 0;
 }
